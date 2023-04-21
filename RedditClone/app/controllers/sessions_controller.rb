9a39cf9a82534ed93989d5640a43dd5d3e74eae0
Jiamin-Zou <jiaminz622@gmail.com>
2023-04-21 14:56:54 -0400

    Added Users & sessions controllers

--- RedditClone/app/controllers/sessions_controller.rb
+++ RedditClone/app/controllers/sessions_controller.rb
@@ -1,13 +1,26 @@
 class SessionsController < ApplicationController
     def new
-
+        @user = User.new
+        render :new
     end
 
     def create
+        username = params[:user][:username]
+        password = params[:user][:password]
 
+        @user = User.find_by_credentials(username, password)
+
+        if @user
+            login!(@user)
+            redirect_to user_url(@user)
+        else
+            flash.now[:errors] = ["Invalid username or password"]
+            render :new
+        end
     end
 
     def destroy
-        
+        logout!
+        redirect_to new_session_url
     end
 end
